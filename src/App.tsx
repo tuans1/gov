973f91e61590cf3464cf54fe4ace28b1f56a4b36
Nguyen Toan Thang <nguyen.toan@ascendcorp.com>
2023-04-20 18:03:48 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,51 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-
+import { Menu, MenuItem } from "@mui/material";
+import logo from "./static/images/twitter-logo.png";
 export default function App() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const open = Boolean(anchorEl);
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 1 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+          <img src={logo} width={40} height={40} alt="Kitty Katty!" />
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1, ml: 2 }}>
+            ChelShit
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button
+            id="basic-button"
+            color="inherit"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+          >
+            User
+          </Button>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+          >
+            <MenuItem onClick={handleClose}>Profile</MenuItem>
+            <MenuItem onClick={handleClose}>My account</MenuItem>
+            <MenuItem onClick={handleClose}>Logout</MenuItem>
+          </Menu>
         </Toolbar>
       </AppBar>
+      <Box component="div" sx={{ display: 'inline' }}>inline</Box>
     </Box>
   );
 }
