ade8dfb7b285e3d0c065af4db5a58f0c74c54f17
Nguyen Toan Thang <nguyen.toan@ascendcorp.com>
2023-06-06 17:36:06 +0700

    update image to text

--- src/components/PDFViewer.js
+++ src/components/PDFViewer.js
@@ -1,111 +1,61 @@
 import React, { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
-
+import Pdf from "./copy.pdf";
+import { Viewer } from "@react-pdf-viewer/core";
+import Tesseract from "tesseract.js";
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.js",
   import.meta.url
 ).toString();
 function PDFViewer() {
   const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(null);
-  const URL =
-    "https://filegovmanagement.s3.ap-southeast-1.amazonaws.com/TfInag6sQ2X3qfU9_3Jun2023130207GMT_1685797327432.Mky6enSxYoHaJ4PP_3Jun2023130207GMT_1685797327419.000.32.63.H49.2011.128.12.pdf";
+  const [pageNumber, setPageNumber] = useState(1);
+  // const URL =
+  //   "https://filegovmanagement.s3.ap-southeast-1.amazonaws.com/TfInag6sQ2X3qfU9_3Jun2023130207GMT_1685797327432.Mky6enSxYoHaJ4PP_3Jun2023130207GMT_1685797327419.000.32.63.H49.2011.128.12.pdf";
 
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-    const docs = [];
-    const docName = "XXX",
-      pages = [];
-    docs.push({
-      name: docName,
-      pages,
-    });
+  const handleDocumentLoad = (e) => {
+    setTimeout(() => {
+      let canvas = document.getElementsByClassName("rpv-core__canvas-layer");
+      let listImg = [];
+      for (var i = 0; i < canvas.length; i++) {
+        listImg.push(canvas[i].firstChild.toDataURL());
+      }
+      listImg.forEach((img, index) => {
+        fetch(img)
+          .then((res) => res.blob())
+          .then((blob) => {
+            const file = new File([blob], "File name" + index, {
+              type: "image/png",
+            });
+            const path = URL.createObjectURL(file);
+            Tesseract.recognize(path, "vie", {
+              logger: (m) => console.log(m),
+            })
+              .catch((err) => {
+                console.error(err);
+              })
+              .then((result) => {
+                // Get Confidence score
+                let confidence = result.confidence;
 
-    pdfjs
-      .getDocument({
-        url: URL,
-        //password: "test",
-      })
-      .promise.then(function (doc) {
-        for (let p = 1; p <= doc.numPages; p++) {
-          const pageInfo = {
-            number: p,
-            name: docName + "-" + p,
-            images: [],
-            svg: {},
-          };
-          pages.push(pageInfo);
-
-          doc.getPage(p).then((page) => console.log(page));
-        }
-      })
-      .catch(function (error) {
-        alert("Failed to open " + docName);
-        console.log(error);
+                let text = result.text;
+                console.log(text);
+                // setText(text);
+              });
+          });
       });
-  }
-  function parsePage(page, pageInfo) {
-    page.getOperatorList().then(function (ops) {
-      console.log("ops", ops);
-      const fns = ops.fnArray,
-        args = ops.argsArray;
-
-      let imgsFound = 0;
-      args.forEach((arg, i) => {
-        //Not a JPEG resource:
-        if (fns[i] !== pdfjs.OPS.paintJpegXObject) {
-          return;
-        }
-
-        console.log("loading", arg);
-        imgsFound++;
-
-        const imgKey = arg[0],
-          imgInfo = {
-            name: pageInfo.name + "-" + imgsFound + ".jpg",
-            url: "",
-          };
-        pageInfo.images.push(imgInfo);
-
-        page.objs.get(imgKey, (img) => {
-          imgInfo.url = img.src;
-        });
-      });
-    });
-
-    //Full SVG:
-
-    // Get viewport (dimensions)
-    const scale = 1.5;
-    const viewport = page.getViewport({ scale });
-
-    pageInfo.svg = {
-      w: viewport.width,
-      h: viewport.height,
-      doc: "",
-    };
-
-    // SVG rendering by PDF.js
-    page
-      .getOperatorList()
-      .then((opList) => {
-        var svgGfx = new pdfjs.SVGGraphics(page.commonObjs, page.objs);
-        return svgGfx.getSVG(opList, viewport);
-      })
-      .then((svg) => {
-        //console.log(svg);
-        pageInfo.svg.doc = svg;
-      });
-  }
+    }, 1000);
+  };
   return (
     <div className="overflow-y-scroll h-[calc(100vh-96px)]">
       <div>
-        <Document file={URL} onLoadSuccess={onDocumentLoadSuccess}>
-          <Page pageNumber={pageNumber} />
-        </Document>
-        <p>
-          Page {pageNumber} of {numPages}
-        </p>
+        <Viewer
+          fileUrl={Pdf}
+          onDocumentLoad={handleDocumentLoad}
+          // renderPage={(renderPageProps) => (
+          //   <CustomPageLayer renderPageProps={renderPageProps} />
+          // )}
+        />
       </div>
     </div>
   );
