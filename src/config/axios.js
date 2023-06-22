54f7aec5ade214bbc1f416d71a775c41c41226ec
Nguyen Toan Thang <nguyen.toan@ascendcorp.com>
2023-06-22 11:04:19 +0700

    update notification and route netlify

--- src/config/axios.js
+++ src/config/axios.js
@@ -15,7 +15,8 @@ axios.interceptors.response.use(
     return response;
   },
   function (error) {
-    createNotification("error", error.message);
+    console.log(error)
+    createNotification("error", error.response.data.message);
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     return Promise.reject(error);
